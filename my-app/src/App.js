f02797cc8d9c2a6477eb8badcf7cb4c2a893b4d1
Leandro Prado <l.prado88@gmail.com>
2021-11-15 22:16:40 -0300

    mi primer commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenidos a Pitt
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ir al Sitio Web
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
