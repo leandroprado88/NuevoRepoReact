dbfb0efc2938e2057918eab6825bedd8e00f8773
Leandro Prado <l.prado88@gmail.com>
2021-11-15 23:49:26 -0300

    segunda entrega

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Navbar from './components/NavBar/NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidos a Pitt
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ir al Sitio Web
-        </a>
-      </header>
+      <Navbar/>
+      <ItemListContainer greeting={'Pitt'}/>
     </div>
   );
 }
---
+++ my-app/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,42 @@
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Iconos from './Iconos';
+
+const NavBar = ()=> {
+    return (
+    <Navbar bg="light" expand="lg">
+    <Container fluid>
+      <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbarScroll" />
+      <Navbar.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+        >
+          <Nav.Link href="#action1">Home</Nav.Link>
+          <Nav.Link href="#action2">Link</Nav.Link>
+          <NavDropdown title="Link" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">
+              Something else here
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets" >
+            <Iconos/>
+          </Nav.Link>
+        </Nav>
+        
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>)
+}
+
+export default NavBar
